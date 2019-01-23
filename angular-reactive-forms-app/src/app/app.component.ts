71e8ede6e5772ee1baa62cbb5958289b2830f4d5
vmurdeshwar <vmurdeshwar@seic.com>
2019-01-23 11:54:54 +0530

    Updated file for validation

--- angular-reactive-forms-app/src/app/app.component.ts
+++ angular-reactive-forms-app/src/app/app.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, FormBuilder,Validators,AbstractControl,ValidatorFn} from '@angular/forms'
+
+function emailMatcher(c:AbstractControl):{[key:string]:boolean} |null{
+
+  const emailControl = c.get('femailAddress');
+  const confirmEmailControl = c.get('fConfirmEmailAddress');
+
+
+if(emailControl.pristine || confirmEmailControl.pristine){
+  return null;
+}
+if(emailControl.value ==confirmEmailControl.value){
+  return null;
+}
+return {'match':true}
+}
+
+
+function ratingRange(min:number, max:number):ValidatorFn{
+  return (c:AbstractControl):{[key:string]:boolean} |null =>{
+   if(c.value!==null && (isNaN(c.value) || c.value<min || c.value>max)){
+     return {'range':true};
+  }
+   return null;
+  }
+}
 
 @Component({
   selector: 'app-root',
-  template: '<employee-list></employee-list> <br/> <br/> Your text goes here : <input type="text" [(ngModel)] = "userInput"> <br/><br/> <simple [simpleInput] ="userInput"></simple>',
+  //template: '<employee-list></employee-list> <br/> <br/> Your text goes here : <input type="text" [(ngModel)] = "userInput"> <br/><br/> <simple [simpleInput] ="userInput"></simple-->',
+  templateUrl: 'app.component.html',
   styleUrls: [ './app.component.sass' ]  
 })
 export class AppComponent  {
@@ -17,6 +44,18 @@ export class AppComponent  {
   lastName :string ="Alter";
   gender : string ='M';
   age : string ='30';
+
+  customerForm: FormGroup;
+  customerFormBuilder:FormBuilder;
+  //Input  n html will bind to this property to display the message
+   emailMessages:string;
+
+
+   private validationMessages={
+     //Key pair. Key is the rule name and teh message to displayed
+     required:'Please enter your emailAddress',
+     email:'Please enter valid emailAddress'
+   }
 
 //simpleComponent Example for component lifecyclc ngChanges, ngOnInit, ngDestroy
 userInput : string;
@@ -34,7 +73,100 @@ fontSize : number =24;
 applyBoldClass:boolean =true
 
 //Class Binding overide any one class
-applyBlueClass:boolean =true
+applyBlueClass:boolean =true;
+constructor(private fb:FormBuilder){
+
+}
+
+//create new instance of the form group
+ngOnInit(){
+  this.customerForm = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    emailAddress: new FormControl(),
+    //set default val
+    sendCatalog: new FormControl(true),
+  });
+
+  
+
+
+   this.customerFormBuilder = this.fb.group({
+    ffirstName: ['',[Validators.required, Validators.minLength(3)]],
+    flastName: ['',[Validators.required, Validators.minLength(3)]],
+  
+    //set default val
+    fsendCatalog: true,
+    emailGroup:this.fb.group({
+      femailAddress: ['',[Validators.required, Validators.email]],
+      fConfirmEmailAddress: ['',Validators.required],
+    },{validator:emailMatcher}),
+    fphone:'',
+    fRating:[null, ratingRange(1,5)],
+    notification:'email'
+
+  });
+   //Use of watcher -- validation control in componet file instead of the html file.
+     this.customerFormBuilder.get('notification').valueChanges.subscribe(
+        value => this.setNotification(value)
+    )
+
+
+    //watcher for email address updates
+    const emailControl = this.customerFormBuilder.get('emailGroup.femailAddress');
+    emailControl.valueChanges.subscribe(
+      value => this.setMessages(emailControl)
+      )
+
+}
+
+populateData():void{
+  this.customerForm.setValue({
+    firstName:"Salman",
+    lastName:"Khan",
+     emailAddress:"test@test.com",
+   sendCatalog:false
+
+  })
+
+}
+
+ populatePatchData():void{
+  this.customerForm.patchValue({
+    firstName:"Shyam",
+    lastName:"Rao",   
+   sendCatalog:true
+
+  })
+ }
+
+setNotification(notifyVia:string):void{
+  const phoneControl =this.customerFormBuilder.get('fphone');
+if(notifyVia=='text'){
+  phoneControl.setValidators(Validators.required);
+}else{
+  phoneControl.clearValidators();
+}
+phoneControl.updateValueAndValidity();
+}
+
+/*This watcher n react is done to remove the html coding done for validation.
+To handle all of this at the component level*/
+setMessages(c:AbstractControl):void{
+  this.emailMessages='';
+  if((c.touched || c.dirty) && c.errors){
+    this.emailMessages= Object.keys(c.errors).map(
+      key => this.emailMessages += this.validationMessages[key]).join('')
+  }
+
+}
+
+
+//saveForm
+save(){
+
+  console.log(this.customerForm);
+}
 
 //Event binding
   showDetails: boolean = false;
--- angular-reactive-forms-app/src/app/app.module.ts
+++ angular-reactive-forms-app/src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { EmployeeComponent } from './employeeApp/employee.components';
-import { EmployeeListComponent } from './employeeApp/employeeList.component';
-import { EmployeeCountComponent } from './employeeApp/employeeCount.component';
-import { CustomPipe } from './employeeApp/customPipe';
 import { SimpleComponent } from './Others/simple.components';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,EmployeeComponent,EmployeeListComponent, EmployeeCountComponent,CustomPipe, SimpleComponent ],
+  imports:      [ BrowserModule, ReactiveFormsModule ],
+  declarations: [ AppComponent, HelloComponent, SimpleComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
